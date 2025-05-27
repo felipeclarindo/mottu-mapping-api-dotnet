6d8d4539ecd8c5c5897b8c4b03ea5ef5b58badb3
felipeclarindo <fglpc2@gmail.com>
2025-05-27 19:54:00 -0300

    feat(structure): Removing files that dont's using.

--- src/Models/Patio.cs
+++
@@ -1,9 +0,0 @@
-namespace Src.Models
-{
-    public class Patio
-    {
-        public required long Id { get; set; }
-        public required string Name { get; set; }
-        public required int MotoQuantity { get; set; }
-    }
-}
